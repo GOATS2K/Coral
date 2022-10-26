8071038723e40d055c3bbbefad7f95af4eee555f
GOATS2K <jp@glattetre.com>
2022-10-26 14:13:49 +0100

    feat: add list of tracks to genre model

--- src/Coral.Database.Models/Genre.cs
+++ src/Coral.Database.Models/Genre.cs
@@ -4,5 +4,6 @@ public class Genre
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public List<Track> Tracks { get; set; } = null!;
     public DateTime DateIndexed { get; set; }
 }

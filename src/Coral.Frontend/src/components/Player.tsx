026c8604b0ef09a6cc65226bd2296cb34ecbacd0
GOATS2K <jp@glattetre.com>
2022-11-16 19:44:40 +0000

    feat(player): add minute marker

--- src/Coral.Frontend/src/components/Player.tsx
+++ src/Coral.Frontend/src/components/Player.tsx
@@ -1,11 +1,19 @@
-import { Button, Flex, Paper, Slider, Text, UnstyledButton } from '@mantine/core';
+import { Group, Paper, Slider, Text, UnstyledButton } from '@mantine/core';
 import React, { useState } from 'react'
 import ReactPlayer from 'react-player';
 import { RepositoryService, TrackDto, TranscodeService } from '../client'
 import { IconPlayerSkipForward, IconPlayerSkipBack, IconPlayerPlay, IconPlayerPause } from '@tabler/icons'
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
+
+dayjs.extend(duration)
 
 async function getTracks(): Promise<TrackDto[]> {
   return await RepositoryService.getApiRepositoryTracks();
+}
+
+function formatSecondsToMinutes(value: number): string {
+  return dayjs.duration(value, "seconds").format('mm:ss')
 }
 
 export default function Player() {
@@ -13,7 +21,7 @@ export default function Player() {
   const [playlist, setPlaylist] = useState("");
   const [playState, setPlayState] = useState(false);
   const [selectedTrack, setSelectedTrack] = useState({} as TrackDto);
-  const [duration, setDuration] = useState(0);
+  // const [duration, setDuration] = useState(0);
   const [secondsPlayed, setSecondsPlayed] = useState(0);
   const playerRef = React.useRef<ReactPlayer>(null);
 
@@ -27,7 +35,7 @@ export default function Player() {
 
   React.useEffect(() => {
     if (tracks?.length === 0 || tracks == null) return;
-    setSelectedTrack(tracks[1])
+    setSelectedTrack(tracks[0])
     if (selectedTrack == null) return;
 
     const getTrackPlaylist = async () => {
@@ -42,26 +50,26 @@ export default function Player() {
       <Paper shadow="xs" radius="md" p="md" style={{
         display: "flex",
         flexDirection: "row",
-        flexWrap: "wrap",
+        flexWrap: "nowrap",
       }}>
         <div style={{
           // vertically center
           margin: "auto 0"
         }}>
-          <Text fz="md">{selectedTrack.title}</Text>
+          <Text fz="md" fw={700}>{selectedTrack.title}</Text>
           <Text fz="xs">{selectedTrack.artist?.name}</Text>
         </div>
         <div style={{
           display: "flex",
           flexDirection: "column",
-          minWidth: "600px",
+          minWidth: "70%",
           // horizontally center
           margin: "0 auto",
         }}>
           <div style={{
             display: "flex",
-            width: "30%",
-            justifyContent: "space-between",
+            maxWidth: "60%",
+            columnGap: "18px",
             // horizontally center
             margin: "0 auto",
             marginBottom: "12px"
@@ -72,16 +80,17 @@ export default function Player() {
             </UnstyledButton>
             <IconPlayerSkipForward size={36}></IconPlayerSkipForward>
           </div>
-          <div>
-            <Slider size={6} value={secondsPlayed} max={duration} onChange={(value) => {
-              console.log("onChange called:", value)
+          <div style={{ minWidth: "75%", display: "flex", flexDirection: "row" }}>
+            <Text mr={16} fz={"sm"}>{formatSecondsToMinutes(secondsPlayed)}</Text>
+            <Slider style={{ flex: 1, margin: "auto 0" }} size={6} value={secondsPlayed} max={selectedTrack.durationInSeconds} onChange={(value) => {
               playerRef.current?.seekTo(value)
               setSecondsPlayed(value)
-            }}></Slider>
+            }} label={(value) => formatSecondsToMinutes(value)}></Slider>
+            <Text ml={16} fz={"sm"}>{formatSecondsToMinutes(selectedTrack.durationInSeconds!)}</Text>
           </div>
         </div>
       </Paper >
-      <ReactPlayer ref={playerRef} url={playlist} playing={playState} onDuration={(playerDuration) => setDuration(playerDuration)} onProgress={(state) => {
+      <ReactPlayer ref={playerRef} url={playlist} playing={playState} onProgress={(state) => {
         setSecondsPlayed(state.playedSeconds)
       }}></ReactPlayer>
     </div >

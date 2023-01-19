81957f7e9d9bf735bdc929b749ba089a330a0b91
GOATS2K <jp@glattetre.com>
2023-01-19 09:02:30 +0000

    feat(playlistItem): add cd animation for when the selected track is being played

--- src/Coral.Frontend/src/components/PlaylistItem.tsx
+++ src/Coral.Frontend/src/components/PlaylistItem.tsx
@@ -1,4 +1,4 @@
-import { IconPlayerPlay } from "@tabler/icons";
+import { IconPlayerPlay, IconDisc, IconVinyl } from "@tabler/icons";
 import { TrackDto } from "../client";
 import styles from "../styles/PlaylistItem.module.css";
 import { Text, UnstyledButton, useMantineTheme } from "@mantine/core";
@@ -13,9 +13,28 @@ type PlaylistItemProps = {
 export function PlaylistItem({ track }: PlaylistItemProps) {
   const [trackHover, setTrackHover] = useState(false);
   const nowPlayingTrack = usePlayerStore((state) => state.selectedTrack);
+  const playState = usePlayerStore((state) => state.playState);
   const setSelectedTrack = (track: TrackDto) =>
     usePlayerStore.setState({ selectedTrack: track });
   const theme = useMantineTheme();
+  const playButton = (
+    <UnstyledButton onClick={() => setSelectedTrack(track)}>
+      <IconPlayerPlay
+        strokeWidth={1}
+        size={24}
+        style={{
+          // center element
+          marginTop: "4px",
+        }}
+      ></IconPlayerPlay>
+    </UnstyledButton>
+  );
+  const trackNumber = <Text fz="lg">{track.trackNumber}</Text>;
+  const discEmoji = (
+    <Text fz="lg" className={styles.spinningDisc}>
+      <IconDisc strokeWidth={1.3} size={24}></IconDisc>
+    </Text>
+  );
 
   return (
     <div
@@ -40,20 +59,15 @@ export function PlaylistItem({ track }: PlaylistItemProps) {
       }
     >
       <div className={styles.trackNumber}>
-        {nowPlayingTrack.id !== track.id && trackHover ? (
-          <UnstyledButton onClick={() => setSelectedTrack(track)}>
-            <IconPlayerPlay
-              strokeWidth={1}
-              size={24}
-              style={{
-                // center element
-                marginTop: "4px",
-              }}
-            ></IconPlayerPlay>
-          </UnstyledButton>
-        ) : (
-          <Text fz="lg">{track.trackNumber}</Text>
-        )}
+        {nowPlayingTrack.id !== track.id && trackHover
+          ? // show play button if track is hovered over
+            // and it is not playing the current selected track
+            playButton
+          : // show the disc emoji for the currently playing track
+          nowPlayingTrack.id === track.id && playState
+          ? discEmoji
+          : // show track if not hovered or currently playing
+            trackNumber}
       </div>
       <div className={styles.info}>
         <Text fz="sm" fw={500}>

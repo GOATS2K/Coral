a11f5d27ff5c97ff675cc04fb979393b28c66021
GOATS2K <jp@glattetre.com>
2023-02-19 00:51:24 +0000

    feat(albumlist): add quick play button on hovering over album artwork

--- src/Coral.Frontend/src/components/AlbumListItem.tsx
+++ src/Coral.Frontend/src/components/AlbumListItem.tsx
@@ -1,10 +1,24 @@
 import React, { useState } from "react";
-import { Anchor, Image, Text } from "@mantine/core";
+import {
+  Anchor,
+  Image,
+  Text,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
 import { SimpleAlbumDto } from "../client/schemas";
 import styles from "../styles/AlbumList.module.css";
 import { getAlbumArtists } from "../common/album";
 import getConfig from "next/config";
 import Link from "next/link";
+import { IconPlayerPlay } from "@tabler/icons";
+import { fetchAlbum } from "../client/components";
+import Album from "../pages/albums/[id]";
+import {
+  Initializer,
+  PlayerInitializationSource,
+  usePlayerStore,
+} from "../store";
 
 type AlbumListItemProps = {
   album: SimpleAlbumDto;
@@ -12,17 +26,57 @@ type AlbumListItemProps = {
 
 export default function AlbumListItem({ album }: AlbumListItemProps) {
   const baseUrl = getConfig().publicRuntimeConfig.apiBaseUrl;
+  const theme = useMantineTheme();
+  const [onHover, setOnHover] = useState(false);
+  const onPlayClick = async (albumToFetch: SimpleAlbumDto) => {
+    // get album
+    let album = await fetchAlbum({
+      pathParams: {
+        albumId: albumToFetch.id,
+      },
+    });
+    let initializer = {
+      id: albumToFetch.id,
+      source: PlayerInitializationSource.Album,
+    } as Initializer;
+    // initialize player
+    usePlayerStore.setState({
+      tracks: album.tracks,
+      initializer: initializer,
+      selectedTrack: album.tracks[0],
+    });
+  };
+  const imageStyle = onHover ? { filter: "brightness(35%)" } : {};
   return (
-    <div className={styles.item}>
-      <Link href={`/albums/${album.id}`} key={album.id.toString()}>
-        <Image
-          className={styles.image}
-          withPlaceholder
-          width={200}
-          height={200}
-          src={`${baseUrl}/api/library/albums/${album.id}/artwork`}
-        ></Image>
-      </Link>
+    <div
+      className={styles.item}
+      onMouseEnter={() => setOnHover(true)}
+      onMouseLeave={() => setOnHover(false)}
+    >
+      <div className={styles.imageContainer}>
+        <Link href={`/albums/${album.id}`} key={album.id.toString()}>
+          <Image
+            className={styles.image}
+            style={imageStyle}
+            withPlaceholder
+            width={150}
+            height={150}
+            src={`${baseUrl}/api/library/albums/${album.id}/artwork`}
+          ></Image>
+        </Link>
+        <div className={styles.playButtonOnImage}>
+          {onHover && (
+            <UnstyledButton onClick={() => onPlayClick(album)}>
+              <div
+                className={styles.circle}
+                style={{ backgroundColor: theme.colors.blue[7] }}
+              >
+                <IconPlayerPlay className={styles.playButton}></IconPlayerPlay>
+              </div>
+            </UnstyledButton>
+          )}
+        </div>
+      </div>
       <div>
         <Link className={styles.link} passHref href={`/albums/${album.id}`}>
           <Anchor

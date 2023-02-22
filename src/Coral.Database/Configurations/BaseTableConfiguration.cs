0fe90c8d9100967716ab29a6de2f433c42944b81
GOATS2K <jp@glattetre.com>
2023-02-22 15:28:47 +0000

    feat(database): automatically set DateIndexed and DateModified

---
+++ src/Coral.Database/Configurations/BaseTableConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Coral.Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coral.Database.Configurations
+{
+    public class BaseTableConfiguration : IEntityTypeConfiguration<BaseTable>
+    {
+        public void Configure(EntityTypeBuilder<BaseTable> builder)
+        {
+            builder.Property(b => b.DateModified)
+                .HasDefaultValue(DateTime.UtcNow)
+                .ValueGeneratedOnUpdate();
+
+            builder.Property(b => b.DateIndexed)
+                .HasDefaultValue(DateTime.UtcNow)
+                .ValueGeneratedOnAdd();
+        }
+    }
+}

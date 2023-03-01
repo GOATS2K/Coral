e89295b597f788979e216cb30a7786ba2291bd39
GOATS2K <jp@glattetre.com>
2023-03-01 11:02:34 +0000

    feat: create showcase page to highlight components for landing page

---
+++ src/Coral.Frontend/src/pages/showcase/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Showcase() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      index
+    </div>
+  );
+}

0b7a512f7e300f78936ce2e49bb4f4ee4a4ac9aa
GOATS2K <jp@glattetre.com>
2023-03-03 17:20:47 +0000

    feat: start on basic plugin

---
+++ src/Coral.Plugin.LastFM/LastFmService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coral.Plugin.LastFM
+{
+    public interface ILastFmService
+    {
+        public string HelloWorld();
+    }
+    public class LastFmService : ILastFmService
+    {
+        public string HelloWorld()
+        {
+            return "Hello world from LastFMService";
+        }
+    }
+}

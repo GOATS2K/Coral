58118013374157968009e041050f7e6d8d22f81c
GOATS2K <jp@glattetre.com>
2023-03-03 17:20:47 +0000

    feat: start on basic plugin

---
+++ src/Coral.Plugin.LastFM/LastFmController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coral.Plugin.LastFM
+{
+    [Route("api/plugin/[controller]")]
+    [ApiController]
+    public class LastFmController : ControllerBase
+    {
+        private readonly ILastFmService _lastFmService;
+
+        public LastFmController(ILastFmService lastFmService)
+        {
+            _lastFmService = lastFmService;
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            return Ok(_lastFmService.HelloWorld());
+        }
+    }
+}
---
+++ src/Coral.Plugin.LastFM/LastFmPlugin.cs
@@ -0,0 +1,11 @@
+﻿using Coral.PluginBase;
+
+namespace Coral.Plugin.LastFM
+{
+    public class LastFMPlugin : IPlugin
+    {
+        public string Name => "Last.fm";
+
+        public string Description => "A simple track scrobbler.";
+    }
+}

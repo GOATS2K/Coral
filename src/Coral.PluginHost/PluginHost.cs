95bf703b5b5c71a4342dc75c9312982e10e7b79a
GOATS2K <jp@glattetre.com>
2023-03-03 17:20:18 +0000

    feat(plugins): add a basic plugin system
    The plugin system works as follows:
    1. PluginHost loads all the assemblies from the plugins directory, ensuring the assembly contains a single class implementing IPlugin.
    2. The types within the loaded assemblies are then registered with the IoC container - including API controllers, making them accessible.

---
+++ src/Coral.PluginHost/PluginHost.cs
@@ -0,0 +1,28 @@
+﻿using Coral.Configuration;
+using Coral.PluginBase;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace Coral.PluginHost
+{
+    public class PluginHost : AssemblyLoadContext
+    {
+        public IEnumerable<Assembly> LoadPluginAssemblies()
+        {
+            var assembliesToLoad = Directory.GetFiles(ApplicationConfiguration.Plugins, "*.dll");
+            var assemblies = assembliesToLoad.Select(a => LoadFromAssemblyPath(a));
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes();
+                foreach (var type in types)
+                {
+                    if (typeof(IPlugin).IsAssignableFrom(type))
+                    {
+                        yield return assembly;
+                    }
+                }
+            }
+        }
+    }
+}

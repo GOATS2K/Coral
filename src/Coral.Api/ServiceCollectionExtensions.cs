1a0c3178b5ade36ac7c0e7b167b7b865b55f7627
GOATS2K <jp@glattetre.com>
2023-04-17 10:18:41 +0100

    refactor: stick service initializer in a extension method

---
+++ src/Coral.Api/ServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Coral.Encoders;
+using Coral.Events;
+using Coral.PluginHost;
+using Coral.Services;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Coral.Api
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<ILibraryService, LibraryService>();
+            services.AddScoped<IIndexerService, IndexerService>();
+            services.AddScoped<ISearchService, SearchService>();
+            services.AddScoped<IArtworkService, ArtworkService>();
+            services.AddScoped<IPaginationService, PaginationService>();
+            services.AddScoped<IPlaybackService, PlaybackService>();
+            services.AddSingleton<IHostServiceProxy, HostServiceProxy>();
+            services.AddSingleton<IPluginContext, PluginContext>();
+            services.AddSingleton<TrackPlaybackEventEmitter>();
+            services.AddSingleton<IServiceProxy, ServiceProxy>();
+            services.AddSingleton<IEncoderFactory, EncoderFactory>();
+            services.AddSingleton<ITranscoderService, TranscoderService>();
+            services.AddSingleton<IActionDescriptorChangeProvider>(MyActionDescriptorChangeProvider.Instance);
+            services.AddSingleton(MyActionDescriptorChangeProvider.Instance);
+        }
+    }
+}

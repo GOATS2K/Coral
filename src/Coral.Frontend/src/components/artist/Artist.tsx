b77288de2dab16d97ae1115f5be75e8562a1053c
GOATS2K <jp@glattetre.com>
2024-07-31 23:36:30 +0100

    feat(artist): sort releases by date

--- src/Coral.Frontend/src/components/artist/Artist.tsx
+++ src/Coral.Frontend/src/components/artist/Artist.tsx
@@ -8,30 +8,30 @@ type ArtistProps = {
 };
 
 export default function Artist({ artist }: ArtistProps) {
-  const singles = artist?.releases.filter((a) => a.type === "Single");
-  const eps = artist?.releases.filter((a) => a.type === "EP");
-  const albums = artist?.releases.filter((a) => a.type === "Album");
-  const miniAlbums = artist?.releases.filter((a) => a.type === "MiniAlbum");
-  const untagged = artist?.releases.filter((a) => a.type == null);
+  const singles = artist?.releases.filter((a) => a.type === "Single").sort((a, b) => b.releaseYear - a.releaseYear);
+  const eps = artist?.releases.filter((a) => a.type === "EP").sort((a, b) => b.releaseYear - a.releaseYear);
+  const albums = artist?.releases.filter((a) => a.type === "Album").sort((a, b) => b.releaseYear - a.releaseYear);
+  const miniAlbums = artist?.releases.filter((a) => a.type === "MiniAlbum").sort((a, b) => b.releaseYear - a.releaseYear);
+  const untagged = artist?.releases.filter((a) => a.type == null).sort((a, b) => b.releaseYear - a.releaseYear);
 
   return (
     <div>
       <ArtistBanner artist={artist} />
       <div className={styles.pageWrapper}>
-        {albums?.length != 0 && <AlbumList albums={albums} title="Releases" />}
+        {albums?.length != 0 && <AlbumList albums={albums} title="Albums" />}
         {miniAlbums?.length != 0 && <AlbumList albums={miniAlbums} title="Mini Albums" />}
         {eps?.length != 0 && <AlbumList albums={eps} title="EPs" />}
         {singles?.length != 0 && <AlbumList albums={singles} title="Singles" />}
 
         {artist?.featuredIn.length != 0 && (
-          <AlbumList albums={artist?.featuredIn} title="Featured In" />
+          <AlbumList albums={artist?.featuredIn.sort((a, b) => b.releaseYear - a.releaseYear)} title="Featured In" />
         )}
 
         {artist?.remixerIn.length != 0 && (
-          <AlbumList albums={artist?.remixerIn} title="Remixer In" />
+          <AlbumList albums={artist?.remixerIn.sort((a, b) => b.releaseYear - a.releaseYear)} title="Remixer In" />
         )}
         {artist?.inCompilation?.length != 0 && (
-          <AlbumList albums={artist?.inCompilation} title="Appears In" />
+          <AlbumList albums={artist?.inCompilation.sort((a, b) => b.releaseYear - a.releaseYear)} title="Appears In" />
         )}
         {untagged?.length != 0 && <AlbumList albums={untagged} title="Unknown Type" />}
       </div>

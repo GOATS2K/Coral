000d9c2b898d3b95caf5ad57d3ac81708713afee
GOATS2K <jp@glattetre.com>
2025-08-14 20:30:18 -0400

    chore: update dependencies

---
+++ src/Coral.Database/Migrations/20250815002908_DbUpdate1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coral.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbUpdate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateIndexed",
+                table: "RecordLabel",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "CURRENT_TIMESTAMP",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateIndexed",
+                table: "RecordLabel",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "CURRENT_TIMESTAMP");
+        }
+    }
+}

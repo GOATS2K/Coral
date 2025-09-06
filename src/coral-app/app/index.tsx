6f027f97ac1c5dd4fe605193c1c2104af4621e0e
GOATS2K <jp@glattetre.com>
2025-09-06 20:18:06 +0200

    chore: initial react-native skeleton

---
+++ src/coral-app/app/index.tsx
@@ -0,0 +1,89 @@
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
+import { THEME } from '@/lib/theme';
+import { Link, Stack } from 'expo-router';
+import { MoonStarIcon, StarIcon, SunIcon } from 'lucide-react-native';
+import { useColorScheme } from 'nativewind';
+import * as React from 'react';
+import { Image, type ImageStyle, View } from 'react-native';
+
+const LOGO = {
+  light: require('@/assets/images/react-native-reusables-light.png'),
+  dark: require('@/assets/images/react-native-reusables-dark.png'),
+};
+
+const SCREEN_OPTIONS = {
+  light: {
+    title: 'React Native Reusables',
+    headerTransparent: true,
+    headerShadowVisible: true,
+    headerStyle: { backgroundColor: THEME.light.background },
+    headerRight: () => <ThemeToggle />,
+  },
+  dark: {
+    title: 'React Native Reusables',
+    headerTransparent: true,
+    headerShadowVisible: true,
+    headerStyle: { backgroundColor: THEME.dark.background },
+    headerRight: () => <ThemeToggle />,
+  },
+};
+
+const IMAGE_STYLE: ImageStyle = {
+  height: 76,
+  width: 76,
+};
+
+export default function Screen() {
+  const { colorScheme } = useColorScheme();
+
+  return (
+    <>
+      <Stack.Screen options={SCREEN_OPTIONS[colorScheme ?? 'light']} />
+      <View className="flex-1 items-center justify-center gap-8 p-4">
+        <Image source={LOGO[colorScheme ?? 'light']} style={IMAGE_STYLE} resizeMode="contain" />
+        <View className="gap-2 p-4">
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            1. Edit <Text variant="code">app/index.tsx</Text> to get started.
+          </Text>
+          <Text className="ios:text-foreground font-mono text-sm text-muted-foreground">
+            2. Save to see your changes instantly.
+          </Text>
+        </View>
+        <View className="flex-row gap-2">
+          <Link href="https://reactnativereusables.com" asChild>
+            <Button>
+              <Text>Browse the Docs</Text>
+            </Button>
+          </Link>
+          <Link href="https://github.com/founded-labs/react-native-reusables" asChild>
+            <Button variant="ghost">
+              <Text>Star the Repo</Text>
+              <Icon as={StarIcon} />
+            </Button>
+          </Link>
+        </View>
+      </View>
+    </>
+  );
+}
+
+const THEME_ICONS = {
+  light: SunIcon,
+  dark: MoonStarIcon,
+};
+
+function ThemeToggle() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
+
+  return (
+    <Button
+      onPressIn={toggleColorScheme}
+      size="icon"
+      variant="ghost"
+      className="rounded-full web:mx-4">
+      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
+    </Button>
+  );
+}

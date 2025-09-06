e0b8a27024778e7f37a5ac2f7b8519d6773be322
GOATS2K <jp@glattetre.com>
2025-09-07 00:06:09 +0200

    fix(rn): fix the god forsaken stupid theme switcher REEEEEEEEEEEEEEEEEE

---
+++ src/coral-app/app/albums/[albumId].tsx
@@ -0,0 +1,37 @@
+import { Text, View } from 'react-native';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import { THEME } from '@/lib/theme';
+import { ThemeToggle } from '@/components/util/theme-toggle';
+import { useColorScheme } from 'nativewind';
+
+const SCREEN_OPTIONS = {
+  light: {
+    title: 'Albums',
+    headerTransparent: true,
+    headerShadowVisible: true,
+    headerStyle: { backgroundColor: THEME.light.background },
+    headerRight: () => <ThemeToggle />,
+  },
+  dark: {
+    title: 'Albums',
+    headerTransparent: true,
+    headerShadowVisible: true,
+    headerStyle: { backgroundColor: THEME.dark.background },
+    headerRight: () => <ThemeToggle />,
+  },
+};
+
+export default function Screen() {
+  const { colorScheme } = useColorScheme();
+  const { albumId } = useLocalSearchParams();
+  console.log(albumId);
+
+  return (
+    <>
+      <Stack.Screen options={SCREEN_OPTIONS[colorScheme ?? 'light']} />
+      <View className="flex-1 items-center justify-center gap-8 p-4">
+        <Text className="dark:text-white">Got album ID: {albumId}</Text>
+      </View>
+    </>
+  );
+}

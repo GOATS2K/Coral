96fb98a2262f7204203a8ecf828d618d00f4bbc2
GOATS2K <jp@glattetre.com>
2025-09-07 04:51:22 +0200

    refactor(db): introduce design-time context factory to create migrations easier

---
+++ src/Coral.Database/CoralDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Coral.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Coral.Database;
+
+public class CoralDbContextFactory : IDesignTimeDbContextFactory<CoralDbContext>
+{
+    private string DbPath { get; set; }
+    
+    public CoralDbContextFactory()
+    {
+        var path = ApplicationConfiguration.AppData;
+        Directory.CreateDirectory(path);
+        DbPath = Path.Join(path, "Coral.db");
+    }
+    
+    public CoralDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CoralDbContext>();
+        optionsBuilder.UseSqlite($"Data Source={DbPath}",
+            opt => opt.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery));
+        return new CoralDbContext(optionsBuilder.Options);
+    }
+}

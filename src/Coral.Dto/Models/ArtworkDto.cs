581fd0697ec67b22dae4abc9a9e11f4c36aeeb4d
GOATS2K <jp@glattetre.com>
2025-09-07 04:53:47 +0200

    feat(artwork): get prominent colors from image

--- src/Coral.Dto/Models/ArtworkDto.cs
+++ src/Coral.Dto/Models/ArtworkDto.cs
@@ -5,5 +5,6 @@ public record ArtworkDto
     public string Small { get; set; } = null!;
     public string Medium { get; set; } = null!;
     public string Original { get; set; } = null!;
+    public string[] Colors { get; set; } = null!;
 
 };

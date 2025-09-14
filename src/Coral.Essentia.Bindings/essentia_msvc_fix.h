771d490c3b9ff89ff66199e4ff322329397ee6e6
GOATS2K <jp@glattetre.com>
2025-09-15 01:01:41 +0200

    feat(essentia.bindings): create wrapper library to be consumed from .NET

--- src/Coral.Essentia.Bindings/essentia_msvc_fix.h
+++
@@ -1,5 +0,0 @@
-// This file is used as a force-include by the MSVC compiler.
-// Its purpose is to load the standard C time header before any
-// conflicting Essentia headers are processed, which resolves
-// the persistent C2039 and C2873 errors related to <ctime>.
-#include <time.h>

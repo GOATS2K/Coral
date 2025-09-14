6517ae5f535ddb69c7bbd7a8a33f6b42e4adf318
GOATS2K <jp@glattetre.com>
2025-09-15 01:01:41 +0200

    feat(essentia.bindings): create wrapper library to be consumed from .NET

--- src/Coral.Essentia.Bindings/essentia_wrapper.h
+++ src/Coral.Essentia.Bindings/essentia_wrapper.h
@@ -1,8 +1,4 @@
 #pragma once
-
-// This header declares the C-style functions that are exported from the
-// EssentiaWrapper DLL for use with P/Invoke in C#.
-
 #ifndef WRAPPER_API
 #if defined(_WIN32)
 #define WRAPPER_API __declspec(dllexport)
@@ -17,8 +13,6 @@ extern "C" {
 
     // --- Function Declarations ---
 
-    WRAPPER_API void ew_init();
-    WRAPPER_API void ew_clean_up();
     WRAPPER_API void* ew_create_mono_loader();
     WRAPPER_API bool ew_configure_mono_loader(void* instance, const char* filename, int sampleRate);
     WRAPPER_API void* ew_create_tf_model();

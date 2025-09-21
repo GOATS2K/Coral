6b78bccbdd70bc54e5595f783d1987170e84b394
GOATS2K <jp@glattetre.com>
2025-09-21 17:59:05 +0200

    refactor: remove old essentia code from codebase

--- src/Coral.Essentia/EssentiaException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Coral.Essentia;
-
-public class EssentiaException : Exception
-{
-    public EssentiaException() { }
-    public EssentiaException(string message) : base(message) { }
-    public EssentiaException(string message, Exception inner) : base(message, inner) { }
-}
